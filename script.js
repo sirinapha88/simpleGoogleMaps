91fdabce29548ca7646816de99763391075d09fe
sirinapha88 <sirinapha.dube@gmail.com>
2016-09-12 09:19:45 -0700

    clean the code

--- script.js
+++ script.js
@@ -215,3 +215,12 @@ function buildIWContent(place) {
     document.getElementById('website-row').style.display = 'none';
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
